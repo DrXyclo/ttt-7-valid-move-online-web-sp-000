cf04d17ca502ef75f83fdb465d28d7f4e70eec79
Dr. XyClo <sdcarmody@gmail.com>
2018-10-27 17:04:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil && board[index] >= 1 && board[index] <= 8
     true
   else
-    falsnilenile
+    nil 
   end
 end
     
