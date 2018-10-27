09f0f285c122f850c664499a887674ba02a9869b
Dr. XyClo <sdcarmody@gmail.com>
2018-10-27 17:04:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil && board[index] >= 1 && board[index] <= 8
     true
   else
-    falsnile
+    falsnilenile
   end
 end
     
