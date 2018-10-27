035568b3ab60e31d581871229579fe3b046099b9
Dr. XyClo <sdcarmody@gmail.com>
2018-10-27 16:21:26 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,21 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if board[index] != " " && board[index] != "" && board[index] != nil
+  if board[index] >= 0 && board[index] <= 8
+    true
+  else
+    false
+  end
+end
+    
+    
+    
+=begin 
+    
+    
+    
+    
+    != " " && board[index] != "" && board[index] != nil
     
     
     
